05b2563b27b0a803f01586b0ac4865551723ec5d
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2019-01-06 14:42:48 +0100

    docs: tune sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -161,7 +161,7 @@ html_theme = 'classic'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -195,15 +195,14 @@ html_theme = 'classic'
 
 # If false, no index is generated.
 #
-# html_use_index = True
+html_use_index = False
 
 # If true, the index is split into individual pages for each letter.
 #
 # html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#
-# html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #

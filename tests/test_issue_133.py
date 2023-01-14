f380d62c249533191077ff9aed4e1b835eaeab3f
Philippe Ombredanne <pombredanne@nexb.com>
2023-01-14 16:00:07 +0100

    Ensure byte tests use bytes
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/test_issue_133.py
+++ tests/test_issue_133.py
@@ -9,26 +9,29 @@
 """
 import ahocorasick
 
+from pytestingutils import conv
+
 
 def test_issue133():
     automaton = ahocorasick.Automaton()
-    automaton.add_word("b", "b")
-    automaton.add_word("abc", "abc")
+    automaton.add_word(conv("b"), "b")
+    automaton.add_word(conv("abc"), "abc")
     automaton.make_automaton()
 
-    res = list(automaton.iter_long("abb"))
+    res = list(automaton.iter_long(conv("abb")))
 
-    expected = [(1, "b"), (2, "b")]
+    expected = [(1, conv("b")), (2, conv("b"))]
     assert res == expected
 
 
 def test_issue133_2():
     automaton = ahocorasick.Automaton()
     for word in ["b", "c", "abd"]:
-        automaton.add_word(word, word)
+        converted = conv(word)
+        automaton.add_word(converted, word)
     automaton.make_automaton()
 
-    res = list(automaton.iter_long("abc"))
+    res = list(automaton.iter_long(conv("abc")))
 
     expected = [(1, "b"), (2, "c")]
     assert res == expected
@@ -37,10 +40,11 @@ def test_issue133_2():
 def test_issue133_3():
     automaton = ahocorasick.Automaton()
     for word in ["知识产权", "国家知识产权局"]:
-        automaton.add_word(word, word)
+        converted = conv(word)
+        automaton.add_word(converted, word)
     automaton.make_automaton()
 
-    res = list(automaton.iter_long("国家知识产权"))
+    res = list(automaton.iter_long(conv("国家知识产权")))
 
     expected = [(5, "知识产权")]
     assert res == expected
